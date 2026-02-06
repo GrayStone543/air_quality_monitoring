3dfb743abb4475031797fa63ab9fffe397a5e18c
GrayStone543 <cf.tsai.009@gmail.com>
2026-02-06 16:48:21 +0800

    Support config file and threshold checking
    1. Using JSON file stored the configuration
    2. Check threshold every 10 seconds

--- code/liveview.py
+++ code/liveview.py
@@ -1,11 +1,23 @@
 import cv2
 import numpy as np
+import json
+import time
 
 
 NO_ACTIVE = "NO ACTIVE"
 HCHO = "HCHO"
 CO = "CO"
 CO2 = "CO2"
+DEFAULT_W = 120
+DEFAULT_H = 54
+DEFAULT_ROI = {
+    HCHO: {"x": 50, "y": 50,  "w": DEFAULT_W, "h": DEFAULT_H, "color": (200, 60, 150), "title": HCHO},
+    CO:   {"x": 50, "y": 250, "w": DEFAULT_W, "h": DEFAULT_H, "color":  (50, 60, 200), "title": CO},
+    CO2:  {"x": 50, "y": 375, "w": DEFAULT_W, "h": DEFAULT_H, "color": (200, 100, 50), "title": CO2},
+    NO_ACTIVE: {"x": 0, "y":0, "w": DEFAULT_W, "h": DEFAULT_H, "color": (128, 128, 128), "title": NO_ACTIVE}
+}
+DEFAULT_WARNING_THRES = {HCHO: 0.098, CO: 35, CO2: 1000}
+
 
 def build_test_frame(width, height):
     frame = np.zeros([height, width, 3], dtype=np.uint8)
@@ -19,44 +31,119 @@ def build_test_frame(width, height):
     return frame
 
 
+def save_config(config):
+    with open("config.json", "w", encoding="utf-8") as f:
+        json.dump(config, f, ensure_ascii=False, indent=4)
+
+def load_config():
+    config = {}
+    try:
+        with open("config.json", "r") as f:
+            config = json.load(f)
+            try:
+                roi = config["roi"]
+            except KeyError:
+                config["roi"] = DEFAULT_ROI
+            try:
+                warning_thres = config["warning threshold"]
+            except KeyError:
+                config["warning threshold"] = DEFAULT_WARNING_THRES
+    except FileNotFoundError:
+        config = {
+            "version": 1.0,
+            "warning threshold": DEFAULT_WARNING_THRES,
+            "roi": DEFAULT_ROI }
+    return config
+
+
 def draw_ROI(img, roi:dict, active=False):
+    fontFace = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale = 0.5
     if active:
         border_color = (0, 250, 0)
     else:
         border_color = roi["color"]
     cv2.rectangle(img, (roi["x"], roi["y"]), (roi["x"] + roi["w"], roi["y"] + roi["h"]), border_color, 2)
-    textSize, baseline = cv2.getTextSize(roi["title"], cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
-    cv2.putText(img, roi["title"], (roi["x"] - textSize[0] - 2, roi["y"] + textSize[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, border_color, 1, cv2.LINE_AA)
+    (text_w, text_h), baseline = cv2.getTextSize(roi["title"], fontFace, fontScale, 1)
+    x = roi["x"] - text_w - 2
+    y = roi["y"] + text_h
+    cv2.putText(img, roi["title"], (x, y), fontFace, fontScale, border_color, 1, cv2.LINE_AA)
+
+
+def draw_elapsed(img, elapsed):
+    PADDING = 8
+    fontFace = cv2.FONT_HERSHEY_PLAIN
+    fontScale = 1.6
+    _sec = int(elapsed % 60)
+    _min = int((elapsed / 60) % 60)
+    _hour = int(elapsed / 3600)
+    _time_str = f"{_hour}:{_min:02}:{_sec:02}"
+    (text_w, text_h), baseline = cv2.getTextSize(_time_str, fontFace, fontScale, 1)
+    _h, _w, _ = img.shape
+    x = _w - text_w - PADDING
+    y = text_h + PADDING
+    x2 = x + text_w
+    y1 = y - text_h - 2
+    cv2.rectangle(img, (x, y1), (x2, y), (32, 32, 32), -1)
+    cv2.putText(img, _time_str, (x, y), fontFace, fontScale, (0, 200, 0), 1, cv2.LINE_AA)
 
 
 def liveview():
+    hcho = 0.0
+    co = 0
+    co2 = 592
+
     liveview_w = 800
     liveview_h = 600
 
+    start_time = time.time()
+    event1_start_time = time.time()
     test_frame = build_test_frame(liveview_w, liveview_h)
 
     step = 1
-    width = 120
-    height = 54
     activeROI = NO_ACTIVE
-    roi = {
-        HCHO: {"x": 50, "y": 50, "w": width, "h": height, "color": (200, 60, 150), "title": HCHO},
-        CO: {"x": 50, "y": 250, "w": width, "h": height, "color": (50, 60, 200), "title": CO},
-        CO2: {"x": 50, "y": 375, "w": width, "h": height, "color": (200, 100, 50), "title": CO2},
-        NO_ACTIVE: {"x": 0, "y":0, "w": width, "h": height}
-    }
+    config = load_config()
+    roi = config["roi"]
+    warning_thres = config["warning threshold"]
 
     while True:
+        elapsed = time.time() - start_time
+
         # read frame from camera lib
         frame = test_frame.copy()
         
         disp = frame.copy()
         
-        # Draw the ROI of HCHO
+        # Iterative action
+        if (time.time() - event1_start_time) >= 10.0:
+            # do OCR every 10 seconds
+            try:
+                # Convert OCR result to float value
+                concentration = {
+                    HCHO: float(hcho),
+                    CO: float(co),
+                    CO2: float(co2)
+                }
+                
+                # send a log to the Google Sheet
+                
+                # check warning threshold here
+                for key in concentration:
+                    if concentration[key] > warning_thres[key]:
+                        notify_msg = f"WARNING!\n{key} ({concentration[key]}) exceed the limit.\n"
+                        #send telegram message
+            except ValueError as e:
+                print("Error: OCR failed")
+            # get the start time for next event
+            event1_start_time = time.time()
+
+        # Draw the ROIs 
         draw_ROI(disp, roi[HCHO], activeROI == HCHO)
         draw_ROI(disp, roi[CO], activeROI == CO)
         draw_ROI(disp, roi[CO2], activeROI == CO2)
-        
+
+        draw_elapsed(disp, elapsed)
+       
         cv2.imshow("Live view", disp)
 
         key = cv2.waitKey(1) & 0xff
@@ -95,6 +182,8 @@ def liveview():
             activeROI = CO
         elif key == ord('3'):
             activeROI = CO2
+        elif key == ord('o'):
+            save_config(config)
         elif key != 0xff:
             print(f'key = {key}')
         
