3d0de361ee1b3b36bd4f9541378d99bf67f101a3
GrayStone543 <cf.tsai.009@gmail.com>
2026-02-25 11:59:23 +0800

    Add more arguments for image processing

--- code/ocr.py
+++ code/ocr.py
@@ -5,7 +5,7 @@ import numpy as np
 import argparse
 
 
-def ssocr_7seg(cv2_img):
+def ssocr_7seg(cv2_img, num_of_digits=-1):
     success, encoded_image = cv2.imencode('.png', cv2_img)
     if not success:
         return "Error: failed to encode image"
@@ -15,7 +15,7 @@ def ssocr_7seg(cv2_img):
     # -C, --omit-decimal-point omit decimal points from output
     # -f, --foreground=COLOR   set foreground color (black or white)
     # -b, --background=COLOR   set background color (black or white)
-    cli_command = ["ssocr", "-d", "-1", "--omit-decimal-point", "-f", "white", "-b", "black", "-"]
+    cli_command = ["ssocr", "-d", f"{num_of_digits}", "-f", "white", "-b", "black", "-"]
 
     try:
         process = subprocess.Popen(
@@ -28,7 +28,7 @@ def ssocr_7seg(cv2_img):
         stdout_data, stderr_data = process.communicate(input=byte_data)
 
         if process.returncode != 0:
-            return f"SSOCR Error: {stderr_data.decode().strip()}"
+            return f"SSOCR Error({process.returncode}): {stderr_data.decode().strip()}"
 
         return stdout_data.decode().strip()
     except subprocess.CalledProcessError as e:
@@ -36,39 +36,40 @@ def ssocr_7seg(cv2_img):
 
 
 def tesseract_7seg(cv2_img):
-    custom_config = r'--oem 3 --psm 7 -c tessedit_char_whitelist=0123456789'
+    custom_config = r'--oem 3 --psm 7 -c tessedit_char_whitelist=0123456789.'
     text = pytesseract.image_to_string(cv2_img, config=custom_config)
     return text
 
 
-def preprocess(image_path):
-    image = cv2.imread(image_path)
-    if image is None:
-        print("Can not read image")
-
+def preprocess(image, using_clahe=True, using_blur=False, binarization=True, morphology=False):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     #gray = cv2.convertScaleAbs(gray, alpha=1.2, beta=10)
 
-    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
-    contrast_enhanced = clahe.apply(gray)
-    #contrast_enhanced = cv2.equalizeHist(gray)
+    if using_clahe:
+        clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+        contrast_enhanced = clahe.apply(gray)
+    else:
+        contrast_enhanced = cv2.equalizeHist(gray)
 
-    blurred = cv2.GaussianBlur(contrast_enhanced, (5, 5), 0)
+    if using_blur:
+        blurred = cv2.GaussianBlur(contrast_enhanced, (5, 5), 0)
+    else:
+        blurred = contrast_enhanced
 
-    _, thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    if binarization:
+        _, thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    else:
+        thresh = blurred
 
-    kernel = np.ones((3, 3), np.uint8)
-    fixed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
+    if morphology:
+        kernel = np.ones((3, 3), np.uint8)
+        fixed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
+    else:
+        fixed = thresh
 
     # optional, enhance again
     #fixed = cv2.dilate(fixed, kernel, iterations=1)
 
-    cv2.imshow('Original', gray)
-    cv2.imshow('Contrast Enhanced', contrast_enhanced)
-    cv2.imshow('Preprocessed', thresh)
-    cv2.imshow('Fixed', fixed)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
     return fixed
 
 if __name__ == "__main__":
@@ -78,13 +79,17 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if type(args.input).__name__ != 'NoneType':
-        cv2_img = preprocess(args.input)
+        img = cv2.imread(args.input)
+        processed_img = preprocess(img, using_clahe=True, using_blur=False, binarization=True, morphology=False)
+        cv2.imshow("processed", processed_img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
         if args.mode == 'ssocr':
             print("----- SSOCR -----")
-            result = ssocr_7seg(cv2_img)
+            result = ssocr_7seg(processed_img, 5)
         else:
             print("----- Tesseract -----")
-            result = tesseract_7seg(cv2_img)
+            result = tesseract_7seg(processed_img)
         print("----- Output -----")
         print(f"{result}")
         print("------------------")
